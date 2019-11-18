48bb38ccea338fdfd19d6d3fce0af016d8b819e2
Kevin Murphy <keggsmurph21@gmail.com>
2019-11-18 02:18:34 -0500

    Basic rewrite of parser
    This implements preliminary parser functionality corresponding to the
    changes from 37c6b407.  Includes some basic test cases (which aren't
    actually checked by anything at the moment).

--- test/parse.py
+++ test/parse.py
@@ -5,6 +5,35 @@ sys.path.insert(0, '../tkml')
 from tkml.lex import lex
 from tkml.parse import parser
 
+def assert_equal(string, expected_tree):
+    parsed_tree = parser.parse(string)
+    print(parsed_tree)
+    return
+    for i, (lexed_token, expected_token) in enumerate(zip(lexed_tokens, expected_tokens)):
+        try:
+            if isinstance(expected_token, tuple):
+                expected_type, expected_value = expected_token
+                assert lexed_token.type == expected_type\
+                        , f'Expected type "{expected_type}", got "{lexed_token.type}"'
+                assert lexed_token.value == expected_value\
+                        , f'Expected value "{expected_value}", got "{lexed_token.value}"'
+            else:
+                expected_type = expected_token
+                assert lexed_token.type == expected_type\
+                        , f'Expected type "{expected_type}", got "{lexed_token.type}"'
+        except AssertionError as e:
+            print(f'Error while lexing "{string}" at token {i}:\n\t{e}')
+            exit()
+
+assert_equal('()', [])
+assert_equal('((@x))', [])
+assert_equal('((@x) (@y) (@z))', [])
+assert_equal('((@x (grid-kwarg a "b")))', [])
+assert_equal('((@x (grid-kwarg a "b") (grid-kwarg c "d")))', [])
+assert_equal('((@x (grid-kwarg a 2)))', [])
+assert_equal('((Button))', [])
+
+'''
 print(parser.parse('(root (defs) (body))'))
 print(parser.parse('(root (defs (@x)) (body))'))
 print(parser.parse('(root (defs (@x) (@y) (@z)) (body))'))
@@ -29,4 +58,11 @@ print(parser.parse('(root (defs) (body @test))'))
 print(parser.parse('(root (defs) (body (label "test")))'))
 print(parser.parse('(root (defs) (body (label @test "test")))'))
 print(parser.parse('(root (defs) (body (div (label "test"))))'))
-#print(parser.parse('(root (defs) (body (button "test"k
+'''
+
+#with open('./examples/ultratrace.tkml') as fp:
+#    for line in fp:
+#        for token in lex(line):
+#            pass
+
+print('all tests passed!')
--- tkml/parse.py
+++ tkml/parse.py
@@ -1,22 +1,6 @@
 import ply.yacc as yacc
 from .tokens import tokens
 
-'''
-class SymbolTable:
-    def __init__(self):
-        self.symbols = dict()
-    def put(self, key, val):
-        if key in self.symbols:
-            raise KeyError(f'key "{key}" already exists in table')
-        self.symbols[key]
-    def get(self, key):
-        return self.symbols.get(key, None)
-    def has(self, key):
-        return self.get(key) is None
-    def clear(self):
-        self.symbols = dict()
-'''
-
 class Node:
     def __init__(self, *children):
         self.children = children
@@ -36,197 +20,94 @@ class Node:
         string += ')'
         return string
 
-class Root(Node):       pass
-class Defs(Node):       pass
-class Def(Node):
-    def __init__(self, name, *children):
-        super().__init__(*children)
-        self.name = name
-class Attr(Node):       pass
-class Rows(Node):       pass
-class Row(Node):        pass
-class Cols(Node):       pass
-class Col(Node):        pass
-class Sticky(Node):     pass
-class Body(Node):       pass
-class Label(Node):      pass
-class Id(Node):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-class Include(Node):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-
-class IdentifiableNode(Node):
-    def __init__(self, name, *children):
-        super().__init__(*children)
-        self.name = name
-
-class Div(IdentifiableNode):        pass
-class Text(IdentifiableNode):       pass
-class Icon(IdentifiableNode):       pass
-class Button(IdentifiableNode):     pass
-class Dropdown(IdentifiableNode):   pass
-class Input(IdentifiableNode):      pass
-class Listbox(IdentifiableNode):    pass
-class Canvas(IdentifiableNode):     pass
-
 def p_root(p):
     '''
-    root        : LPAREN ROOT defs body RPAREN
+    root        : LPAREN abstractnodes RPAREN
     '''
-    p[0] = Root(p[3], p[4])
+    p[0] = Node(*p[2])
 
-def p_defs(p):
+def p_abstractnodes(p):
     '''
-    defs        : LPAREN DEFS defnode RPAREN
-    '''
-    p[0] = Defs(*p[3])
-
-def p_defnode(p):
-    '''
-    defnode     : def defnode
-                | empty
+    abstractnodes   : abstractnode abstractnodes
+                    | empty
     '''
     if len(p) == 3:
-        p[0] = [p[1], *p[2]]
+        p[0] = Node(p[1], *p[2])
     else:
         p[0] = []
 
-def p_def(p):
+def p_abstractnode(p):
     '''
-    def         : LPAREN INCLUDE IDENTIFIER attrnode RPAREN
+    abstractnode    : include
+                    | widget
     '''
-    p[0] = Def(p[3], *p[4])
-
-def p_attrnode(p):
-    '''
-    attrnode    : attr attrnode
-                | empty
-    '''
-    if len(p) == 3:
-        p[0] = [p[1], *p[2]]
-    else:
-        p[0] = []
-
-def p_attr(p):
-    '''
-    attr        : LPAREN IDENTIFIER STRLITERAL RPAREN
-    '''
-    p[0] = Attr(p[2], p[3])
-
-def p_body(p):
-    '''
-    body        : LPAREN BODY divnode RPAREN
-    '''
-    p[0] = Body(*p[3])
-
-def p_divnode(p):
-    '''
-    divnode     : empty
-    '''
-    p[0] = []
-
-def p_include_divnode(p):
-    '''
-    divnode     : include divnode
-    '''
-    p[0] = [p[1], *p[2]]
-
-def p_div_divnode(p):
-    '''
-    divnode     : div divnode
-    '''
-    p[0] = [p[1], *p[2]]
-
-def p_div(p):
-    '''
-    div         : LPAREN DIV id includes divnode RPAREN
-    '''
-    p[0] = Div(p[3], *p[4], *p[5])
-
-def p_label_divnode(p):
-    '''
-    divnode     : label divnode
-    '''
-    p[0] = [p[1], *p[2]]
-
-def p_label(p):
-    '''
-    label       : LPAREN LABEL includes STRLITERAL RPAREN
-    '''
-    p[0] = Label(*p[3], p[4])
+    p[0] = p[1]
 
 def p_includes(p):
     '''
-    includes     : include includes
-                 | empty
+    includes    : include includes
+                | empty
     '''
     if len(p) == 3:
-        p[0] = [p[1], *p[2]]
+        p[0] = Node(p[1], *p[2])
     else:
         p[0] = []
 
 def p_include(p):
     '''
-    include     : INCLUDE IDENTIFIER
+    include      : LPAREN INCLUDEIDENTIFIER includeargs RPAREN
     '''
-    p[0] = Include(p[2])
+    p[0] = Node(p[2], *p[3])
 
-def p_id(p):
+def p_includeargs(p):
     '''
-    id          : ID IDENTIFIER
+    includeargs : includearg includeargs
                 | empty
     '''
     if len(p) == 3:
-        p[0] = Id(p[2])
+        p[0] = Node(p[1], *p[2])
     else:
-        p[0] = None
+        p[0] = []
+
+def p_includearg(p):
+    '''
+    includearg  : LPAREN BAREIDENTIFIER BAREIDENTIFIER STRLITERAL RPAREN
+                | LPAREN BAREIDENTIFIER BAREIDENTIFIER INTLITERAL RPAREN
+    '''
+    p[0] = Node(p[2], p[3], p[4])
+
+def p_widgets(p):
+    '''
+    widgets     : widget widgets
+                | empty
+    '''
+    if len(p) == 3:
+        p[0] = Node(p[1], *p[2])
+    else:
+        p[0] = []
+
+def p_widget(p):
+    '''
+    widget      : LPAREN WIDGETIDENTIFIER widgetargs RPAREN
+    '''
+    print(p)
+    p[0] = Node(p[2], *p[3])
+
+def p_widgetargs(p):
+    '''
+    widgetargs  : empty
+    '''
+    if len(p) == 3:
+        p[0] = Node(p[1], *p[2])
+    else:
+        p[0] = []
 
 def p_empty(p):
-    'empty :'
-    pass
+    '''
+    empty       :
+    '''
 
 def p_error(p):
     raise ValueError(f'Parse error in input: {p}')
 
 parser = yacc.yacc()
-
-# these can be during AST construction
-#parser.ids = SymbolTable()
-#parser.classes = SymbolTable()
-#parser.vars = SymbolTable()
-'''
-id          : ( ID STRLITERAL )
-            | empty
-class       : ( CLASS STRLITERAL ) class
-            | empty
-
-div         : ( DIV id class divnode )
-divnode     : text divnode
-            | icon divnode
-            | label divnode
-            | button divnode
-            | dropdown divnode
-            | input divnode
-            | listbox divnode
-            | canvas divnode
-            | empty
-
-text        : ( TEXT STRLITERAL )
-icon        : ( ICON STRLITERAL )
-label       : ( LABEL id class text )
-button      : ( BUTTON id class text )
-            | ( BUTTON id class icon )
-dropdown    : ( DROPDOWN id class var )
-input       : ( INPUT id class var )
-listbox     : ( LISTBOX id class )
-canvas      : ( CANVAS id class )
-
-var         : ( VAR varid )
-
-empty       :
-'''
-
